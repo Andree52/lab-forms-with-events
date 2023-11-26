f958ac864c70652faacdb743545402362891d022
Andree52 <andreearter@pursuit.org>
2023-11-26 16:57:37 -0500

    forms-events

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log("Code your solution!")
+function handleTodoFormSubmit(event) {
+    event.preventDefault();
+  
+    const inputValue = document.getElementById('todo-input').value.trim();
+    const errorMessage = document.getElementById('error-message');
+  
+    if (inputValue === '') {
+      errorMessage.style.display = 'block';
+      errorMessage.textContent = 'Please enter a valid to-do!';
+      
+      setTimeout(() => {
+        errorMessage.style.display = 'none';
+      }, 3000);
+    } else {
+      const newTodoItem = document.createElement('li');
+      newTodoItem.textContent = inputValue;
+  
+      newTodoItem.addEventListener('click', function() {
+        this.classList.toggle('completed');
+      });
+  
+      document.getElementById('todo-list').appendChild(newTodoItem);
+      document.getElementById('todo-input').value = '';
+    }
+  }
+  
+  document.getElementById('todo-form').addEventListener('submit', handleTodoFormSubmit);
+  
+console.log("handletodoformSubmit")
